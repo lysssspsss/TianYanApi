cb2b2a74a554dd494bc41bc55915ef4d43e0ab91
535578445@qq.com <535578445@qq.com>
2018-09-11 17:44:26 +0800

    debug

--- application/index/controller/Base.php
+++ application/index/controller/Base.php
@@ -276,7 +276,7 @@ class Base extends Controller
                 $path = $path.'.mp4';
                 $filename = $filename.'.mp4';
             }
-            var_dump($path_local_file,$cover);exit;
+            //var_dump($path_local_file,$cover);exit;
             Tools::getVideoCover($path_local_file,1,$cover);//获取截图
             $is1 = Tools::UploadFile_OSS($cover_path,$cover);//上传截图到oss
             $is2 = Tools::UploadFile_OSS($path,$path_local_file);//上传视频到oss
